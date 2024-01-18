83d3f41b0a2350aee818a88f87a76c58ae14d9b6
Ryusei Kojima <ryusei@Ryuseis-MacBook-Air.local>
2024-01-18 22:29:34 +0900

    fix intents

--- src/resources/views/clips/index.blade.php
+++ src/resources/views/clips/index.blade.php
@@ -1,15 +1,20 @@
-
 @extends('layouts.app')
 
-@section('content')
-<h1 class="">記事一覧</h1>
-<div class="fs-3">記事一覧</div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>記事一覧</title>
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+  <!-- Styles -->
+  @vite('resources/css/app.css')
+</head>
 
-  <div>
-    <button type="submit" class="btn btn-primary my-1">新規登録</button>
-  </div>
-</form>
-<table border="1">
+@section('content')
+  <div class="fs-3">記事一覧</div>
+  <button type="submit" class="btn btn-primary my-1">新規登録</button>
+  <table border="1">
     <tr>
       <th>映画タイトル</th>
       <th width="150px">画像URL</th>
@@ -23,14 +28,14 @@
         <td><img style="max-width: 150px;" src="{{ $movie->image_url }}" alt=""></td>
         <td>{{ $movie->published_year }}</td>
         <td>
-          @if ( $movie->is_showing )
+          @if ($movie->is_showing)
             上映中
           @else
             上映予定
           @endif
         </td>
         <td>{{ $movie->description }}</td>
-      </td>
+        </td>
     @endforeach
   </table>
 @endsection
