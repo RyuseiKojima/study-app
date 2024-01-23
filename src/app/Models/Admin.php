46cc5c0f71651d2dc0c26cd58bcba1bc874fcffc
Ryusei Kojima <ryusei@Ryuseis-MacBook-Air.local>
2024-01-24 06:30:09 +0900

    0124

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -1,12 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// login周りのModelは以下で実装する必要あり
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -14,18 +10,13 @@ use App\Notifications\AdminResetPassword as ResetPasswordNotification;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens;
-    use HasFactory;
-    // メールを送れるようにする
-    use Notifiable;
-    use ResetPasswordNotification;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    // createやupdateを受け付ける
     protected $fillable = [
         'name',
         'email',
@@ -47,7 +38,6 @@ class Admin extends Authenticatable
      *
      * @var array<string, string>
      */
-    // DBから得られたデータを自動変換する
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
--- src/app/Notifications/AdminResetPassword.php
+++ src/app/Notifications/AdminResetPassword.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Illuminate\Auth\Notifications;
+namespace App\Notifications;
 
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
 
-class ResetPassword extends Notification
+class AdminResetPassword extends Notification
 {
     /**
      * The password reset token.
