00facbfdd45d5d26ae3a23ce3c3679286ecba382
Ryusei Kojima <jiko.likegc@gmail.com>
2024-02-24 19:50:34 +0900

    add gitignore

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; //追記
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +26,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // ここから追記
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        // ここまで追記
         if (request()->is('admin/*')) {
             config(['session.cookie' => config('session.cookie_admin')]);
         }
     }
 }
+// check deploy
